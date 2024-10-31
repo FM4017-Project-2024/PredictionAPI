0fd9da46afd84de529b1bf5f28b9aa3d1cdb0e73
Kim Langvannskås <Kimlangv@hotmail.com>
2024-10-31 15:10:58 +0100

    added small features

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/PredictionAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/PredictionAPI.AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PredictionAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+95b1b648970030c324aaa3fb6cf3f20f9d0ea024")]
 [assembly: System.Reflection.AssemblyProductAttribute("PredictionAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PredictionAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
